8a937178b6a8389438753fd171653d6a3829dcbc
Alex Pshul <alex@pshul.com>
2020-04-11 23:30:39 +0300

    Update the service to read from the mock json file

--- src/Features/FunctionAppDetails/FunctionAppExecutionsService.ts
+++ src/Features/FunctionAppDetails/FunctionAppExecutionsService.ts
@@ -1,9 +1,10 @@
 import { from } from "rxjs";
 import { delay } from "rxjs/operators";
+import MockOrchestrators from '../../assets/mocks/MockOrchestrators.json';
 
 export class FunctionAppExecutionService {
   public static getFunctionAppExecutions(functionAppId: string): Promise<string[]> {
-    const executions: string[] = ["EXEC 1_" + functionAppId, "EXEC 2_" + functionAppId, "EXEC 3_" + functionAppId];
+    const executions = MockOrchestrators.map(orchestrator => `${orchestrator.functionName} (${functionAppId})`);
 
     return from([executions])
       .pipe(
