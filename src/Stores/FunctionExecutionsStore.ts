950430c27f95c7413072c1e114c8c80a60a2ad24
Alex Pshul <alex@pshul.com>
2020-04-16 02:09:51 +0300

    Added clear to selection and refactored to use rxjs in Mobx store to fix refetching issue.

--- src/Stores/FunctionExecutionsStore.ts
+++ src/Stores/FunctionExecutionsStore.ts
@@ -1,7 +1,11 @@
 import { ChildStore } from "./Core";
-import { observe, observable, action } from "mobx";
+import { observable, action } from "mobx";
 import { FunctionAppExecutionService } from "../Features/FunctionAppDetails/FunctionAppExecutionsService";
 import { FunctionExecutionModel } from "../Models/FunctionExecution.model";
+import { toStream } from "mobx-utils";
+import { from } from "rxjs";
+import { tap, switchMap, map } from "rxjs/operators";
+import { FunctionExecutionDto } from "../Dtos/FunctionExecution.dto";
 
 export class FunctionExecutionsStore extends ChildStore {
   @observable isLoading: boolean = false;
@@ -9,18 +13,17 @@ export class FunctionExecutionsStore extends ChildStore {
   @observable selectedExecutionId: string = "";
 
   initialize() {
-    observe(
-      this.root.functionApps,
-      "selectedFunctionAppId",
-      async change => {
-        this.isLoading = true;
-        const executionDtos = await FunctionAppExecutionService.getFunctionAppExecutions(change.newValue);
-        this.executions = executionDtos.map(({ endTime, ...rest }) => {
-          return {
-            ...rest,
-            isRunning: !endTime
-          }
-        })
+    // The following line is broken in strict mode due to mobx-utils toStream and rxjs from combination.
+    // @ts-ignore
+    from(toStream(() => this.root.functionApps.selectedFunctionAppId))
+      .pipe(
+        tap(_ => this.isLoading = true),
+        tap(_ => this.selectedExecutionId = ""),
+        switchMap((functionId: string) => FunctionAppExecutionService.getFunctionAppExecutions(functionId)),
+        map((executionDtos: FunctionExecutionDto[]) => executionDtos.map(({ endTime, ...rest }) => ({ ...rest, isRunning: !endTime })))
+      )
+      .subscribe((executionModels: FunctionExecutionModel[]) => {
+        this.executions = executionModels;
         this.isLoading = false;
       });
   }
