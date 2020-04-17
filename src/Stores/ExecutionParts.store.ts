7481ec6c5dc3bff2df05cfff15a71677f5da41d0
Alex Pshul <alex@pshul.com>
2020-04-17 15:14:42 +0300

    Filtered null execution IDs

--- src/Stores/ExecutionParts.store.ts
+++ src/Stores/ExecutionParts.store.ts
@@ -2,7 +2,7 @@ import { ChildStore } from "./Core";
 import { toObservable } from "../Core/Utils/MobxUtils";
 import { observable } from "mobx";
 import { ExecutionPartsModel } from "../Models/ExecutionPart.model";
-import { switchMap, tap, map } from "rxjs/operators";
+import { switchMap, tap, map, filter } from "rxjs/operators";
 import { ExecutionPartsService } from "../Services/ExecutionParts.service";
 import { ExecutionPartsDto } from "../Dtos/ExecutionPart.dto";
 
@@ -14,6 +14,7 @@ export class ExecutionPartsStore extends ChildStore {
   initialize() {
     toObservable(() => this.root.executions.selectedExecutionId)
       .pipe(
+        filter(executionId => !!executionId),
         tap(_ => this.isLoading = true),
         switchMap(executionId => ExecutionPartsService.getExecutionParts(executionId)),
         map(executionPartsDto => this.convertToModel(executionPartsDto))
